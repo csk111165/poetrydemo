d675f7f3ad74d542e72e7560ce951a06fe401d5e
Chandra Shekhar <cshekhar@nvidia.com>
2023-06-18 08:40:03 +0530

    adding a new function

--- myproject/addition.py
+++ myproject/addition.py
@@ -4,3 +4,6 @@ Just adds the numbers
 
 def add_numbers(a, b):
     return a+b
+
+def add_three_numbers(a, b, c):
+    return a+b+c
--- tests/test_addition.py
+++ tests/test_addition.py
@@ -4,4 +4,4 @@ Test module for the addition
 from myproject.addition import add_numbers
 
 def test_additoin():
-    assert add_numbers(1,2) == 3
+    assert add_numbers(1,2) == 3 
